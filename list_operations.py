dde231768a148dca60d5d21a6afe9c61bfbffab2
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-03 12:51:12 -0700

    Fixed Replace_third_and_last

--- list_operations.py
+++ list_operations.py
@@ -156,7 +156,7 @@ def replace_head(input_list):
 
     
     input_list[0] = 42
-     
+
 
 
 
@@ -172,7 +172,8 @@ def replace_third_and_last(input_list):
 
     """
 
-    pass
+    input_list[2] = 37
+    input_list[-1] = 37
 
 
 def replace_middle(input_list):
